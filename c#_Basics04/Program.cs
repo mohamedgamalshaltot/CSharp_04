3404cc43392600c2eb0b080deef3d2180d1d1808
mohamedgamalshaltot <mohmedgamalshaltot@gmail.com>
2026-09-07 12:28:52 +0300

    Question 07

--- c#_Basics04/Program.cs
+++ c#_Basics04/Program.cs
@@ -7,7 +7,7 @@
             #region Question 01
             //Given string title = "Refactoring";, write a switch statement that prints "Great choice!" if the title is "Clean Code", "Nice pick!" if it's "Refactoring", and "Never heard of it" for anything else.
             string title = "Refactoring";
-            switch(title) 
+            switch (title)
             {
                 case "Clean Code":
                     Console.WriteLine("Great choice!");
@@ -35,7 +35,7 @@
             #endregion
             #region Question 04
             //Using the same books array, use a while loop to print every book title. 
-            string[] books2= { "Clean Code", "The Pragmatic Programmer", "Refactoring" };
+            string[] books2 = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };
             int index = 0;
             while (index < books.Length)
             {
@@ -45,8 +45,8 @@
             #endregion
             #region Question 05
             //Write a do-while loop that prints "Checking book..." exactly 3 times.
-             int count = 0;
-            do 
+            int count = 0;
+            do
             {
                 Console.WriteLine("Checking book...");
                 count++;
@@ -60,6 +60,19 @@
                 Console.WriteLine(book);
             }
             #endregion
+            #region Question 07
+            //Using the same books array, loop through it and print each title, but stop completely (break) once you reach "Refactoring". 
+            string[] books4 = { "Clean Code", "The Pragmatic Programmer", "Refactoring" };
+            foreach (string book in books4)
+            {
+                Console.WriteLine(book);
+                if (book == "Refactoring")
+                {
+                    break;
+                }
+            }
+            #endregion
+
 
         }
     }
